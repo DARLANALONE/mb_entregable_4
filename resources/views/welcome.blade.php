6d9819ca668beae78233ab57d61eebc499d13d28
Mrluisdavido <80801130+Mrluisdavido@users.noreply.github.com>
2021-06-07 11:00:11 -0500

    actualizacion_del_home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -101,12 +101,9 @@
                 <nav class="navigation visible_menu"><ul id="menu-navigation" class="menu"><li id="menu-item-373" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-home current-menu-ancestor current-menu-parent menu-item-has-children menu-item-373"><a href="http://promo-theme.com/medel/"><span>Home</span></a>
                             <ul class="sub-menu">
                                 <li id="menu-item-459" class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-117 current_page_item menu-item-459"><a href="https://promo-theme.com/medel/dental/" aria-current="page"><span>Dental</span></a></li>
-                                <li id="menu-item-378" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home menu-item-378"><a href="https://promo-theme.com/medel/"><span>Doctor personal</span></a></li>
-                                <li id="menu-item-377" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-377"><a href="https://promo-theme.com/medel/healthy-food/"><span>Healthy Food</span></a></li>
+                                <li id="menu-item-378" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home menu-item-378"><a href="https://promo-theme.com/medel/"><span>personal medico</span></a></li>
                                 <li id="menu-item-376" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-376"><a href="https://promo-theme.com/medel/private-clinic/"><span>Private Clinic</span></a></li>
                                 <li id="menu-item-375" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-375"><a href="https://promo-theme.com/medel/surgery/"><span>Surgery</span></a></li>
-                                <li id="menu-item-374" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-374"><a href="https://promo-theme.com/medel/yoga/"><span>Yoga</span></a></li>
-                                <li id="menu-item-552" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-552"><a href="https://promo-theme.com/medel/home-shop/"><span>login</span></a></li>
                             </ul>
                         </li>
                         <li id="menu-item-426" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-426"><a href="#"><span>Blog</span></a>
